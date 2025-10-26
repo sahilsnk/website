bf2186a28a24e5e4e2bbb26c6328d4a8357594ed
sahilsanaik <sahilsantoshnaik3112004wed@gmail.com>
2025-10-26 19:02:08 +0530

    to sublte down

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -248,7 +248,7 @@ export default function Index() {
   </p>
   <hr className="my-8 border-portfolio-text-light/30 w-1/2 mx-auto" />
   <p className="text-xl text-portfolio-text-light/80 max-w-2xl mx-auto">
-    Want to see my resume? Just type <strong>resume</strong>
+    Want to see my resume? Just tap 5 times or type <strong>resume</strong>
   </p>
           </div>
           <div className="flex flex-col md:flex-row items-center justify-center gap-8">
